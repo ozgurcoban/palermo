6ef71e9e879ee16f2d7c9bdd359ee34149f526ed
Abdohm-coder <hadjmedabdo@gmail.com>
2024-01-08 16:48:26 +0100

    feat: hero section
    add FadeUp & MotionDiv components for animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,52 +1,56 @@
-'use client';
+// "ues client";
+import Image from "next/image";
+import MotionDiv from "./ui/MotionDiv";
+import FadeUp from "./ui/FadeUp";
 
-import Image from 'next/image';
-import { useRef } from 'react';
-import { MotionValue, useScroll, useTransform, motion } from 'framer-motion';
-import { useTheme } from 'next-themes';
-import clsx from 'clsx';
-import { useTranslations } from 'next-intl';
+const variants = {
+  initial: {
+    scale: 0.5,
+    opacity: 0,
+  },
+  animate: {
+    scale: 1,
+    opacity: 1,
+  },
+};
 
 export default function Hero() {
-  const t = useTranslations('IndexPage');
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const { resolvedTheme } = useTheme();
-
-  const overlayStyle = clsx(
-    'absolute inset-0 z-10 bg-black',
-    resolvedTheme === 'dark' ? ' opacity-75' : 'opacity-50'
-  );
-
-  function useParallax(value: MotionValue<number>, distance: number) {
-    return useTransform(value, [0, 1], [-distance, distance]);
-  }
-
-  const y = useParallax(scrollYProgress, 400);
-
   return (
-    <div className='min-h-dvh'>
-      <div className={overlayStyle} />
-      <motion.div
-        style={{ y }}
-        className='absolute inset-0 flex items-center justify-center z-0'
+    <div className="relative h-screen w-screen container">
+      <MotionDiv
+        className="relative max-h-[calc(100vh-132px)] h-full overflow-hidden"
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        transition={{
+          duration: 0.2,
+          ease: "linear",
+          type: "spring",
+          damping: 16,
+          stiffness: 100,
+        }}
       >
+        <div className="absolute z-10 w-full h-full bg-black/40" />
         <Image
-          src='/hero.jpeg'
-          alt='hero'
-          fill
-          style={{ objectFit: 'cover' }}
+          src="/hero.jpeg"
+          alt="hero"
+          width={1920}
+          height={1080}
+          style={{ objectFit: "cover" }}
+          className="h-full w-full"
         />
-      </motion.div>
-      <div className='absolute inset-0 flex flex-col items-center justify-center z-20'>
-        <div className='bg-accent rounded p-10'>
-          <h1
-            ref={ref}
-            className='max-w-screen-sm text-secondary-foreground text-4xl'
-          >
-            {t('hero.title')}
+      </MotionDiv>
+      <div className="absolute sm:px-8 px-4 text-center inset-0 flex flex-col items-center justify-center z-20">
+        <FadeUp delay={0.3} duration={0.3}>
+          <p className="title-secondary !normal-case text-gray-200 max-w-[600px] !font-normal">
+            At <u>Palermo</u> where tasty dishes and a fine selection of beers
+          </p>
+        </FadeUp>
+        <FadeUp delay={0.5} duration={0.5}>
+          <h1 className="text-light lg:text-[180px] sm:text-[160px] text-[20vw] leading-[120px] sm:leading-[135px]  font-lobster break-words">
+            Welcome everyone
           </h1>
-        </div>
+        </FadeUp>
       </div>
     </div>
   );
---
+++ src/components/ui/FadeUp.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import MotionDiv from "./MotionDiv";
+
+type FadeUp = {
+  children: React.ReactNode;
+  delay?: number;
+  duration?: number;
+  className?: string;
+};
+
+const variants = {
+  initial: {
+    y: 10,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+const FadeUp: React.FC<FadeUp> = ({ children, className, delay, duration }) => {
+  return (
+    <MotionDiv
+      variants={variants}
+      initial={"initial"}
+      animate="animate"
+      className={className}
+      transition={{ delay, duration, ease: "easeIn" }}
+    >
+      {children}
+    </MotionDiv>
+  );
+};
+
+export default FadeUp;
