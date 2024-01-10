c34067c468a5a874d057ab155f821963e9a896ef
Abdohm-coder <hadjmedabdo@gmail.com>
2024-01-10 08:11:13 +0100

    style: add fadeUp to the "see more" button in gallery section

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import FadeUp from "./ui/FadeUp";
-import { HeartFilledIcon, HeartIcon } from "@radix-ui/react-icons";
+import { HeartFilledIcon } from "@radix-ui/react-icons";
 
 const Gallery = () => {
+  const gallery = [...Array(10)];
+
   return (
     <section className="w-screen py-40 h-full ">
       <div className="container flex flex-col items-center">
@@ -18,7 +20,7 @@ const Gallery = () => {
         </FadeUp>
         <div className="relative w-full">
           <div className="mt-36 w-full grid auto-rows-[250px] lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-            {[...Array(10)].map((_, i) => {
+            {gallery.map((_, i) => {
               const index = i % 10;
               return (
                 <FadeUp
@@ -31,18 +33,20 @@ const Gallery = () => {
                       ? "lg:col-span-1 sm:col-span-2"
                       : ""
                   }`}
-                ></FadeUp>
+                />
               );
             })}
           </div>
-          <button
-            title="See more"
-            aria-live="polite"
-            className="absolute border z-10 bottom-0 left-0 w-full h-20 backdrop-blur-sm bg-light/30 text-rose-500 rounded-xl overflow-hidden flex items-center justify-center gap-2"
-          >
-            <span className="font-lato text-xl">See more</span>
-            <HeartFilledIcon width={20} height={20} />
-          </button>
+          <FadeUp delay={0.05 * gallery.length}>
+            <button
+              title="See more"
+              aria-live="polite"
+              className="absolute border z-10 bottom-0 left-0 w-full h-20 backdrop-blur-sm bg-light/30 text-rose-500 rounded-xl overflow-hidden flex items-center justify-center gap-2"
+            >
+              <span className="font-lato text-xl">See more</span>
+              <HeartFilledIcon width={20} height={20} />
+            </button>
+          </FadeUp>
         </div>
       </div>
     </section>
