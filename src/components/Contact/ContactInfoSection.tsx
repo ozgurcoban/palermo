5f89f3f1e48c66234fabd702b26afcbce74aaa60
Özgür Coban <infuser-mitosis0q@icloud.com>
2024-01-10 21:11:17 +0100

    feat: Complete Contact Form design layout
    The design layout for the Contact Form is finished. The logic implementation is still in progress.

--- src/components/Contact/ContactInfoSection.tsx
+++ src/components/Contact/ContactInfoSection.tsx
@@ -3,14 +3,11 @@ import {
   ChatBubbleIcon,
   EnvelopeClosedIcon,
 } from "@radix-ui/react-icons";
-import Form from "./Form";
+import ContactForm from "./Form";
 
 const openingHours = [
   { day: "Monday:", open: "11:00 AM", close: "01:00 AM" },
-  { day: "Tuesday:", open: "11:00 AM", close: "03:00 AM" },
-  { day: "Wednesday:", open: "11:00 AM", close: "03:00 AM" },
-  { day: "Thursday:", open: "11:00 AM", close: "03:00 AM" },
-  { day: "Friday:", open: "11:00 AM", close: "03:00 AM" },
+  { day: "Tuesday - Friday:", open: "11:00 AM", close: "03:00 AM" },
   { day: "Saturday:", open: "12:00 PM", close: "03:00 AM" },
   { day: "Sunday:", open: "12:00 PM", close: "01:00 AM" },
 ];
@@ -19,8 +16,8 @@ export default function ContactInfoSection() {
   return (
     <section className="w-screen py-16 bg-[#F8EEE2]">
       <div className="container grid lg:grid-cols-2 items-center gap-5">
-        <div className="flex justify-between md:flex-row lg:flex-col flex-col gap-10">
-          <div>
+        <div className="flex justify-between md:flex-row lg:flex-col flex-col gap-10 mb-10">
+          <div className="flex-1">
             <h2 className="title-secondary">where to find us</h2>
             <ul>
               <li className="flex items-center gap-2 pt-3">
@@ -37,7 +34,7 @@ export default function ContactInfoSection() {
               </li>
             </ul>
           </div>
-          <div>
+          <div className="flex-1">
             <h2 className="title-secondary">opening hours</h2>
             <ul>
               {openingHours.map((day, i) => (
@@ -54,7 +51,7 @@ export default function ContactInfoSection() {
             </ul>
           </div>
         </div>
-        <Form />
+        <ContactForm />
       </div>
     </section>
   );
--- src/components/Contact/Form.tsx
+++ src/components/Contact/Form.tsx
@@ -1,7 +1,104 @@
-export default function Form() {
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+import * as z from "zod";
+
+import { ContactFormSchema } from "@/lib/ContactFormSchema";
+import { sendMail } from "@/app/_actions";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
+
+export type ContactFormInputs = z.infer<typeof ContactFormSchema>;
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    reset,
+  } = useForm<ContactFormInputs>({ resolver: zodResolver(ContactFormSchema) });
+
+  const processForm: SubmitHandler<ContactFormInputs> = async data => {
+    const result = await sendMail(data);
+
+    if (result?.success) {
+      console.log({ data: result.data });
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      });
+      reset();
+      return;
+    }
+    console.log(result?.error);
+    toast({
+      variant: "destructive",
+      title: "Uh oh! Something went wrong.",
+      description: "There was a problem with your request.",
+    });
+  };
   return (
-    <form className="self-start">
-      <h2 className="title-secondary">get in touch</h2>
+    <form onSubmit={handleSubmit(processForm)} className="self-start">
+      <h2 className="title-secondary ">get in touch</h2>
+      <div className="flex gap-4 pb-4 pt-3">
+        {/* Name field */}
+        <div className="flex-1">
+          <input
+            className=" border-2 border-accent w-full p-2"
+            placeholder="name"
+            {...register("name")}
+          />
+          {errors.name?.message && (
+            <p className="ml-1 mt-1 text-sm text-destructive">
+              {errors.name.message}
+            </p>
+          )}
+        </div>
+
+        {/* Email field */}
+        <div className="flex-1">
+          <input
+            className="flex-1 border-2 border-accent w-full p-2"
+            placeholder="email"
+            {...register("email")}
+          />
+          {errors.email?.message && (
+            <p className="ml-1 mt-1 text-sm text-destructive">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+      </div>
+      {/* Message field */}
+      <div className="">
+        <textarea
+          rows={5}
+          placeholder="message"
+          className="border-2 border-accent w-full p-2"
+          {...register("message")}
+        />
+        {errors.message?.message && (
+          <p className="ml-1 text-sm text-destructive">
+            {errors.message.message}
+          </p>
+        )}
+      </div>
+      <Button type="submit" className="mt-4 bg-dark">
+        Submit
+      </Button>
     </form>
   );
 }
